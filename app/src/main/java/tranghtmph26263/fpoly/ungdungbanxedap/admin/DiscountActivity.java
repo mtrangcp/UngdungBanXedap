1a55c77b529d03994ccd69b264ca62910312f391
mtrangcp <haminhtrang545@gmail.com>
2023-11-18 02:22:51 +0700

    add_product

--- app/src/main/java/tranghtmph26263/fpoly/ungdungbanxedap/admin/DiscountActivity.java
+++ app/src/main/java/tranghtmph26263/fpoly/ungdungbanxedap/admin/DiscountActivity.java
@@ -1,16 +1,23 @@
 package tranghtmph26263.fpoly.ungdungbanxedap.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import tranghtmph26263.fpoly.ungdungbanxedap.R;
 
 public class DiscountActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discount);
+
+
+
+
+
     }
 }
